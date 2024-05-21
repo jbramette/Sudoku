608c1ab1878cdcf251a42f7487942d085f234361
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2024-05-21 22:38:04 +0200

    Ajoutez des fichiers projet.

---
+++ Sudoku/FormHome.vb
@@ -0,0 +1,25 @@
+﻿Public Class FormHome
+    Private Sub FormHome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+
+        Dim r As MsgBoxResult = MsgBox("You are about to leave, please confirm exit", vbOKCancel Or vbExclamation, "Exit")
+
+        If r = MsgBoxResult.Ok Then
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub btnPlayGame_Click(sender As Object, e As EventArgs) Handles btnPlayGame.Click
+        Dim nickname As String = cbxName.Text
+
+        If nickname.Length <= 0 Then
+            MsgBox("No nickname selected", vbOK Or vbInformation)
+        End If
+
+        cbxName.Items.Add(nickname)
+    End Sub
+End Class
