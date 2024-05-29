9d817ef142372e77ce75c6d102b2d7d61ec2fe2a
Harlay <leo.harlay@etu.u-paris.fr>
2024-05-29 15:07:22 +0200

    Implementer JSON deserializing pour le puzzle

--- Sudoku/PuzzleGenerator/ApiFetcher.vb
+++ Sudoku/PuzzleGenerator/ApiFetcher.vb
@@ -4,6 +4,18 @@ Module ApiFetcher
 
     Private Const DUSOKU_API_LINK As String = "https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value}}}"
 
+    Private Class PuzzleResponse
+        Public Property newboard As NewBoard
+    End Class
+
+    Private Structure NewBoard
+        Public Property grids As List(Of Value)
+    End Structure
+
+    Private Structure Value
+        Public Property value As List(Of List(Of Integer))
+    End Structure
+
     Private Function HttpGet() As String
         Using webClient As New Net.WebClient
             Return webClient.DownloadString(DUSOKU_API_LINK)
@@ -12,9 +24,9 @@ Module ApiFetcher
 
     Public Function FetchPuzzle() As List(Of List(Of Integer))
         Dim response = HttpGet()
-        Dim json = JsonSerializer.Deserialize(Of Object)(response)
+        Dim json = JsonSerializer.Deserialize(Of PuzzleResponse)(response)
 
-        ' TODO
+        Return json.newboard.grids(0).value
     End Function
 
 End Module
