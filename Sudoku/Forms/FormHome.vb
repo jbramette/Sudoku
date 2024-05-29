dec73f64d3f7dfbe82900fa501c1fbc40051c344
Harlay <leo.harlay@etu.u-paris.fr>
2024-05-29 13:32:06 +0200

    You can start the game by pressing enter key

--- Sudoku/Forms/FormHome.vb
+++ Sudoku/Forms/FormHome.vb
@@ -14,6 +14,14 @@
 
     End Sub
 
+    Private Sub OnEnterPress(sender As Object, e As KeyPressEventArgs) Handles cbxName.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            e.Handled = True
+            OnPlayClick(sender, e)
+        End If
+    End Sub
+
+
     Private Sub OnPlayClick(sender As Object, e As EventArgs) Handles btnPlayGame.Click
         Dim nickname As String = GetNickname()
 
@@ -25,4 +33,5 @@
         Me.Hide()
         FormGame.Show()
     End Sub
+
 End Class
